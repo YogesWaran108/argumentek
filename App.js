cc80e53714d23cdd9327b8e2072ed6a69a143136
YogesWaran108 <87996389+YogesWaran108@users.noreply.github.com>
2022-09-22 09:12:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+          <Routes>
+
+          <Route index element={<Home/>} />
+
+          <Route path="/services" element={<Services/>} />
+          <Route path="/products" element={<Products/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+          
+        </Routes>
+      
+      </BrowserRouter>
+      </>
+  );
+}
+
+export default App;
